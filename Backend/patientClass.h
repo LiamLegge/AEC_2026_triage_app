3f63c5b26e789844698f3dc04e6a5ca7f64d0357
ryan6625 <rmleblanc05@gmail.com>
2026-01-17 11:54:33 -0400

    test

--- Backend/patientClass.h
+++ Backend/patientClass.h
@@ -9,6 +9,9 @@
 
 using namespace std;
 
+const string Admin_Email = "hospital_email@domain.ca";
+const string Admin_Password = "password123";
+
 class patient{
     private:
         unsigned int Patient_ID;
@@ -18,7 +21,6 @@ class patient{
         string Birth_Day;
         string Health_Card;
         string Email;
-        string Birth_Day;
         string Chief_Complaint;
         unsigned int Triage_Level;
         string Accessibility_Profile;
@@ -45,52 +47,53 @@ class patient{
             UI_Setting = UI;
             Language = Lang;
             Check_In = get_Current_Time(); // format: Fri Jan 17 10:52:30 2026
-            Internal_Time = get_Current_Time();
+            Internal_Time = UTC_to_int(get_Current_Time());
         }
 
         void set_Patient_ID(int ID){Patient_ID = ID;}
         int get_Patient_ID(){return Patient_ID;}
 
         void set_Name(string N){Name = N;}
-        string get_Name(){return Name;}
+        string get_Name()const{return Name;}
 
         void set_Age(int A){Age = A;}
-        int get_Age(){return Age;}
+        int get_Age()const{return Age;}
 
         void set_Sex(char S){Sex = S;}
-        char get_Sex(){return Sex;}
+        char get_Sex()const{return Sex;}
 
         void set_Email(string E){Email = E;}
-        string get_Email(){return Email;}
+        string get_Email()const{return Email;}
 
         void set_Birth_Day(string BD){Birth_Day = BD;}
-        string get_Birth_Day(){return Birth_Day;}
+        string get_Birth_Day()const{return Birth_Day;}
 
         void set_Health_Card(string HC){Health_Card = HC;}
-        string get_Health_Card() {return Health_Card;}        
+        string get_Health_Card()const{return Health_Card;}        
 
-        void set_Cheif_Complaint(string CC){Chief_Complaint = CC;}
-        string get_Cheif_Complaint() {return Chief_Complaint;}
+        void set_chief_Complaint(string CC){Chief_Complaint = CC;}
+        string get_chief_Complaint()const{return Chief_Complaint;}
 
         void set_Triage_Level(int TL){Triage_Level = TL;}
-        int get_Triage_Level(){return Triage_Level;}
+        int get_Triage_Level()const{return Triage_Level;}
 
         void set_Accessibility_Profile(string AP){Accessibility_Profile = AP;}
-        string get_Accessibility_Profile(){return Accessibility_Profile;}
+        string get_Accessibility_Profile()const{return Accessibility_Profile;}
 
         void set_Prefered_Mode(string PM){Prefered_Mode = PM;}
-        string get_Prefered_Mode(){return Prefered_Mode;}
+        string get_Prefered_Mode()const{return Prefered_Mode;}
 
         void set_UI_Setting(string UI){UI_Setting = UI;}
-        string get_UI_Setting(){return UI_Setting;}
+        string get_UI_Setting()const{return UI_Setting;}
 
         void set_Language(string Lang){Language = Lang;}
-        string get_Language(){return Language;}
+        string get_Language()const{return Language;}
 
         string get_Check_In_Full(){return Check_In;}
-        int get_Check_In_Time(){return UTC_to_int(Check_In);}
+        int get_Check_In_Time()const{return UTC_to_int(Check_In);}
 
         void set_Internal_Time(int T){Internal_Time = T;}
+        int get_Internal_Time()const{return Internal_Time;}
         
 };
 string get_Current_Time(void){
@@ -115,28 +118,28 @@ void Update_Sevarity(patient p){
     int IT = p.get_Internal_Time();
     //increase sevarity based on time waiting
     switch(p.get_Triage_Level()){
-        Case 1:
+        case 1:
             return; // no need to update
-        Case 2:
+        case 2:
             //Update after an hour of waiting
             if(IT >= CT +  10000){
                 p.set_Triage_Level(1);
                 p.set_Internal_Time(CT);
                 generate_email(p);
             }
-        Case 3://Update after an 1.5 hours of waiting
+        case 3://Update after an 1.5 hours of waiting
             if(IT >= CT +  13000){
                 p.set_Triage_Level(2);
                 p.set_Internal_Time(CT);
                 generate_email(p);
             }
-        Case 4://Update after an 2 hours of waiting
+        case 4://Update after an 2 hours of waiting
             if(IT >= CT +  20000){
                 p.set_Triage_Level(2);
                 p.set_Internal_Time(CT);
                 generate_email(p);
             }
-        Case 5://Update after an 2.5 hours of waiting
+        case 5://Update after an 2.5 hours of waiting
             if(IT >= CT +  23000){
                 p.set_Triage_Level(2);
                 p.set_Internal_Time(CT);
@@ -150,13 +153,13 @@ void generate_email(patient p){
     //check if no email
     if(p.get_Email() == "NULL"){return;}
     string command = "powershell -Command \"Send-MailMessage "
-        "-From 'hospital@admin.com' "
+        "-From '" + Admin_Email + "' "
         "-To '" + p.get_Email() + "' "
         "-Subject 'Triage Level Updated' "
         "-Body 'Your Triage Priority has been updated to " + to_string(p.get_Triage_Level()) + " . Thank you for your patience' "
         "-SmtpServer 'smtp.gmail.com' "
         "-Port 587 "
-        "-Credential (New-Object System.Management.Automation.PSCredential('hostpital@gmail.com',(ConvertTo-SecureString 'your_password' -AsPlainText -Force))) "
+        "-Credential (New-Object System.Management.Automation.PSCredential('" + Admin_Email + "',(ConvertTo-SecureString '" + Admin_Email + "' -AsPlainText -Force))) "
         "-UseSsl\"";
 
     system(command.c_str());
